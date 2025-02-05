951b9beeb90ae264145f72d0843d795801a2b336
project_Html <khoanguyenduc99@gmail.com>
2025-02-05 08:43:02 +0700

    reuse button.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,13 @@
 import { Text, View, StyleSheet } from "react-native"
+import ShareButton from "components/button/share.button";
+import { APP_COLOR } from "utils/constant";
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         borderColor: "red",
-        borderWidth: 5
+        borderWidth: 5,
     },
     welcomeText: {
         flex: 0.6,
@@ -17,7 +20,8 @@ const styles = StyleSheet.create({
     welcomeBtn: {
         flex: 0.4,
         borderColor: "grey",
-        borderWidth: 5
+        borderWidth: 5,
+        gap: 20
     },
     heading: {
         fontSize: 40,
@@ -25,7 +29,7 @@ const styles = StyleSheet.create({
     },
     body: {
         fontSize: 30,
-        color: "orange",
+        color: APP_COLOR.ORANGE,
         marginVertical: 10,
     },
     footer: {
@@ -59,21 +63,75 @@ const WelcomePage = () => {
                     Nền tảng giao đồ ăn trực tuyến hàng đầu Việt Nam
                 </Text>
             </View>
-            <View style={styles.welcomeBtn}>
-                <Text>
-                    Đăng nhập với
-                </Text>
-                <View>
-                    <View style={styles.btnContainer}>
-                        <View style={styles.btnContent}>
-                            <Text style={styles.btnText}>Facebook</Text>
-                        </View>
 
-                    </View>
-                    <View><Text>Google</Text></View>
+            <View style={styles.welcomeBtn}>
+                <View style={{
+                    borderBottomWidth: 1,
+                    borderBottomColor: "red",
+                    marginHorizontal: 50,
+                }}>
+                    <Text style={{
+                        padding: 10,
+                        textAlign: "center",
+                        backgroundColor: "white",
+                        alignSelf: "center",
+                        position: "relative",
+                        top: 20
+                    }}>
+                        Đăng nhập với
+                    </Text>
                 </View>
-                <View><Text>Đăng nhập với email</Text></View>
-                <View><Text>Chưa có tài khoản? Đăng ký.</Text></View>
+                <View style={{
+                    flexDirection: "row",
+                    justifyContent: "center",
+                    gap: 30
+                }}>
+                    <ShareButton
+                        title="faceBook"
+                        onPress={() => { alert("me") }}
+                        textStyle={{ textTransform: "uppercase" }}
+                        btnStyle={{
+                            justifyContent: "center",
+                            borderRadius: 30,
+                            backgroundColor: "#fff"
+                        }}
+                        icons={
+                            <FontAwesome5 name="facebook" size={30} color="black" />
+                        }
+                    />
+
+                    <ShareButton
+                        title="google"
+                        onPress={() => { alert("me") }}
+                        textStyle={{ textTransform: "uppercase" }}
+                        btnStyle={{
+                            justifyContent: "center",
+                            borderRadius: 30,
+                            paddingHorizontal: 20,
+                            backgroundColor: "#fff"
+                        }}
+                        icons={
+                            <FontAwesome5 name="google" size={30} color="black" />
+                        }
+                    />
+
+                </View>
+                <View>
+                    <ShareButton
+                        title="Đăng nhập với email"
+                        onPress={() => { alert("me") }}
+                        textStyle={{ color: "#fff", paddingVertical: 5 }}
+                        btnStyle={{
+                            justifyContent: "center",
+                            borderRadius: 30,
+                            marginHorizontal: 50,
+                            paddingVertical: 10,
+                            backgroundColor: "#2c2c2c"
+                        }}
+                        pressStyle={{ alignSelf: "stretch" }}
+                    />
+                </View>
+                <View><Text style={{ textAlign: "center" }}>Chưa có tài khoản? Đăng ký.</Text></View>
             </View>
         </View>
     )
---
+++ src/components/button/share.button.tsx
@@ -0,0 +1,45 @@
+import { Pressable, StyleProp, StyleSheet, Text, TextStyle, View } from "react-native";
+import { ReactNode } from "react";
+import { APP_COLOR } from "utils/constant";
+
+const styles = StyleSheet.create({
+    btnContainer: {
+        borderRadius: 10,
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        flexDirection: "row",
+        gap: 10,
+        alignItems: "center",
+        backgroundColor: APP_COLOR.ORANGE
+    }
+})
+interface IProps {
+    title: string;
+    onPress: () => void;
+
+    textStyle?: StyleProp<TextStyle>;
+    pressStyle?: StyleProp<TextStyle>;
+    btnStyle?: StyleProp<TextStyle>;
+    icons?: ReactNode
+}
+
+const ShareButton = (props: IProps) => {
+    const { title, onPress, textStyle, pressStyle, btnStyle, icons } = props;
+    return (
+        <Pressable
+            style={({ pressed }) => ([{
+                opacity: pressed === true ? 0.5 : 1,
+                alignSelf: "flex-start", //fit-content
+            }, pressStyle])}
+            onPress={onPress}
+        >
+            <View style={[styles.btnContainer, btnStyle]}>
+                {icons}
+
+                <Text style={textStyle}>{title}</Text>
+            </View>
+        </Pressable>
+    )
+}
+
+export default ShareButton;
