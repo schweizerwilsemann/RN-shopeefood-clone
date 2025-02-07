e6e54d1d4798d59d2c38f005bc2c62709f908ce0
project_Html <khoanguyenduc99@gmail.com>
2025-02-07 14:39:43 +0700

    Design verify OTP page

--- src/app/(auth)/verify.tsx
+++ src/app/(auth)/verify.tsx
@@ -1,12 +1,48 @@
+import { APP_COLOR } from '@/utils/constant'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
+import OTPTextView from 'react-native-otp-textinput'
 
+
+const styles = StyleSheet.create({
+    container: {
+        paddingVertical: 30,
+        paddingHorizontal: 20
+    },
+    header: {
+        fontSize: 25,
+        fontWeight: '600',
+        marginVertical: 20
+    }
+})
 const verify = () => {
     return (
-        <View>
-            <Text>
-                verify page
-            </Text>
+        <View style={styles.container}>
+            <Text style={styles.header}>Xác thực tài khoản</Text>
+            <Text style={{ marginVertical: 10 }}>Vui lòng nhập mã xác nhận đã gửi đến địa chỉ Email: </Text>
+            <View style={{ marginVertical: 20 }}>
+                <OTPTextView
+                    // ref={input}
+                    // containerStyle={styles.textInputContainer}
+                    // handleTextChange={setOtpInput}
+                    // handleCellTextChange={handleCellTextChange}
+                    inputCount={6}
+                    keyboardType="numeric"
+                    tintColor={APP_COLOR.ORANGE}
+                    textInputStyle={{
+                        borderWidth: 1,
+                        borderColor: APP_COLOR.GRAY,
+                        borderBottomWidth: 1,
+                        borderRadius: 5,
+                        // @ts-ignore:next-line
+                        color: APP_COLOR.ORANGE
+                    }}
+                />
+            </View>
+            <View>
+                <Text>Không nhận được mã xác nhận?</Text>
+                <Text style={{ textDecorationLine: "underline", fontWeight: "600" }}> Gửi lại</Text>
+            </View>
         </View>
     )
 }
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,44 +1,50 @@
 import { Stack } from "expo-router";
 import { RootSiblingParent } from 'react-native-root-siblings';
-
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const RootLayout = () => {
     return (
         <RootSiblingParent>
-            <Stack
-                screenOptions={{
-                    headerStyle: {
-                        backgroundColor: '#f4511e',
-                    },
-                    headerTintColor: '#fff',
-                    headerTitleStyle: {
-                        fontWeight: 'bold',
-                    },
-                }}
-            >
-                <Stack.Screen
-                    name="index"
-                    options={{ headerShown: false }}
-                />
-                <Stack.Screen
-                    name="(auth)/signup"
-                    options={{ headerShown: false }}
-                />
-                <Stack.Screen
-                    name="(tabs)"
-                    options={{ headerTitle: "Trang chủ" }}
-                />
+            <SafeAreaView style={{ flex: 1 }}>
+                <Stack
+                    screenOptions={{
+                        headerStyle: {
+                            backgroundColor: '#f4511e',
+                        },
+                        headerTintColor: '#fff',
+                        headerTitleStyle: {
+                            fontWeight: 'bold',
+                        },
+                    }}
+                >
+                    <Stack.Screen
+                        name="index"
+                        options={{ headerShown: false }}
+                    />
+                    <Stack.Screen
+                        name="(auth)/signup"
+                        options={{ headerShown: false }}
+                    />
+                    <Stack.Screen
+                        name="(auth)/verify"
+                        options={{ headerShown: false }}
+                    />
+                    <Stack.Screen
+                        name="(tabs)"
+                        options={{ headerTitle: "Trang chủ" }}
+                    />
 
-                <Stack.Screen
-                    name="product/index"
-                    options={{ headerTitle: "Sản phẩm" }}
-                />
+                    <Stack.Screen
+                        name="product/index"
+                        options={{ headerTitle: "Sản phẩm" }}
+                    />
 
-                <Stack.Screen
-                    name="(auth)/login"
-                    options={{ headerTitle: "Đăng nhập" }}
-                />
-            </Stack>
+                    <Stack.Screen
+                        name="(auth)/login"
+                        options={{ headerTitle: "Đăng nhập" }}
+                    />
+                </Stack>
+            </SafeAreaView>
         </RootSiblingParent>
     )
 }
